1f4f8c1135541431fda185b8a25ccd740552d261
ragavapalani <ragavapalani73@gmail.com>
2022-02-15 11:02:16 +0530

    message

---
+++ src/org/ragava/Palani.java
@@ -0,0 +1,15 @@
+package org.ragava;
+
+public class Palani extends Ragava{
+	private void palaniSkill() {
+		System.out.println("palani skills is photography");
+
+	}
+	
+public static void main(String[] args) {
+	Palani a=new Palani();
+	a.employeeId();
+	a.employeeName();
+	a.palaniSkill();
+}
+}
---
+++ src/org/ragava/Ragava.java
@@ -0,0 +1,18 @@
+package org.ragava;
+
+public class Ragava {
+
+	public void employeeId() {
+	System.out.println("employee id is 8973");
+	
+	}
+	
+	public void employeeName() {
+		System.out.println("employee name is ragava");
+
+	}
+
+	
+	
+	
+}
